32f48902ea7bf4dfa791edd5dc1df5392fdd5c6c
Altuğ Yilmaz <altuyilmaz@gmail.com>
2024-08-16 13:46:53 +0300

    Frontend for left-sidebar settings is done. backend is connected to settings. processing backend code will be added soon.

--- website/user_settings.py
+++ website/user_settings.py
@@ -6,11 +6,31 @@ from .processor import char_len_flash_error_notification
 
 user_settings = Blueprint("user_settings",__name__)
 
-@user_settings.route("/profiles/<username>/change-name", methods=["POST","GET"])
+@user_settings.route("/profiles/<username>/preferences", methods=["POST","GET"])
 @login_required
-def change_name(username):
+def preferences(username):
     if username != current_user.username:
         flash("You cannot access this page.",category="error")
         return redirect(url_for("views.home"))
     else:
         return "<h1>Function Works</h1>"
+
+@user_settings.route("/profiles/<username>/edit-profile", methods = ["POST","GET"])
+def edit_profile(username):
+    return "<h1>Function Works</h1>"
+
+@user_settings.route("/profiles/<username>/change_password", methods = ["POST","GET"])
+def change_password(username):
+    return "<h1>Function Works</h1>"
+
+@user_settings.route("/profiles/<username>/account-history", methods = ["POST","GET"])
+def account_history(username):
+    return "<h1>Function Works</h1>"
+
+@user_settings.route("/profiles/<username>/language", methods = ["POST","GET"])
+def language(username):
+    return "<h1>Function Works</h1>"
+
+@user_settings.route("/profiles/<username>/account-freeze", methods = ["POST","GET"])
+def account_freeze_delete(username):
+    return "<h1>Function Works</h1>"
